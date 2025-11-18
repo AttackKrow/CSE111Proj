feef0a039155a92e31944e2ffa10d44aa2005aa2
AttackKrow <forcenicholas@gmail.com>
2025-11-17 17:17:57 -0800

    Proper init

--- tableCreation.sql
+++ tableCreation.sql
@@ -1,3 +1,28 @@
+CREATE TABLE Customers (
+    c_id INT PRIMARY KEY ,
+    c_name VARCHAR(100) NOT NULL,
+    c_email VARCHAR(100) UNIQUE NOT NULL,
+    c_phone VARCHAR(15)
+);
+
+CREATE TABLE Employees (
+    e_id INT PRIMARY KEY ,
+    e_name VARCHAR(100) NOT NULL,
+    e_position VARCHAR(50)
+);
+
+CREATE TABLE Payments (
+    p_id INT PRIMARY KEY ,
+    p_amount DECIMAL(10, 2),
+    p_c_id INT NOT NULL,
+    p_e_id INT NOT NULL,
+    p_method VARCHAR(50),
+    p_transactiondate DATETIME,
+    FOREIGN KEY (p_c_id) REFERENCES Customers(c_id),
+    FOREIGN KEY (p_e_id) REFERENCES Employees(e_id)
+);
+
+
 CREATE TABLE Rentals (
     r_id INT PRIMARY KEY ,
     r_c_id INT NOT NULL,
@@ -12,29 +37,6 @@ CREATE TABLE Rentals (
     FOREIGN KEY (r_e_id) REFERENCES Employees(e_id)
 );
 
-CREATE TABLE Payments (
-    p_id INT PRIMARY KEY ,
-    p_amount DECIMAL(10, 2),
-    p_c_id INT NOT NULL,
-    p_e_id INT NOT NULL,
-    p_method VARCHAR(50),
-    p_transactiondate DATETIME,
-    FOREIGN KEY (r_c_id) REFERENCES Customers(c_id),
-    FOREIGN KEY (r_e_id) REFERENCES Employees(e_id)
-);
-
-CREATE TABLE Customers (
-    c_id INT PRIMARY KEY ,
-    c_name VARCHAR(100) NOT NULL,
-    c_email VARCHAR(100) UNIQUE NOT NULL,
-    c_phone VARCHAR(15)
-);
-
-CREATE TABLE Employees (
-    e_id INT PRIMARY KEY ,
-    e_name VARCHAR(100) NOT NULL,
-    e_position VARCHAR(50)
-);
 
 CREATE TABLE Bikes (
     b_id INT PRIMARY KEY ,
