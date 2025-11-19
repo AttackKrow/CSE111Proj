ae415a3307d2dfe520dca1e63ed5604a63fe8ff7
AttackKrow <forcenicholas@gmail.com>
2025-11-18 16:48:38 -0800

    up to 10 and 17

--- scripts/queries.py
+++ scripts/queries.py
@@ -122,6 +122,48 @@ QUERIES = {
                 "SELECT * FROM Rentals WHERE r_c_id = (SELECT c_id FROM Customers WHERE c_email = ?)",
                 "SELECT * FROM Rental_Bikes WHERE rb_r_id IN (SELECT r_id FROM Rentals WHERE r_c_id = (SELECT c_id FROM Customers WHERE c_email = ?))" ],
         'params': ['Email Address']
-    }
+    },
+    #7
+    '7': {
+        'label':'Find Customers Most Recent Rental by Email',
+        'sql':  "SELECT * FROM Rentals WHERE r_c_id = (SELECT c_id FROM Customers WHERE c_email = ?) ORDER BY r_startdate DESC LIMIT 1",
+        'params': ['Email Address']
+    },
+    #8
+    '8': {
+        'label':'View Mainenance History for a Bike by Bike ID',
+        'sql':  """SELECT * FROM Maintenance WHERE m_b_id = ?""",
+        'params': ['Bike ID']
+    },
+    #9
+    '9': {
+        'label':'Find Available Bikes for a Given Date/Time Range',
+        'sql':  """SELECT B.* FROM Bikes B 
+        WHERE B.b_type = ?
+        AND NOT EXISTS (
+            SELECT 1 FROM Rentals R
+            JOIN Rental_Bikes RB ON R.r_id = RB.rb_r_id
+            WHERE RB.rb_b_id = B.b_id
+            AND R.r_startdate < ?
+            AND R.r_startdate > ?
+            )""",
+        'params': ['Bike Type', 'Rental End Date/Time (YY-MM-DD HH)', 'Rental Start Date/Time (YY-MM-DD HH)']
+    },
+    #10
+    '10': {
+        'label':'Add new Employee',
+        'sql':  """INSERT INTO Employees (e_name, e_position) VALUES (?, ?)""",
+        'params': ['Name', 'Position']
+    },
 
+
+
+    #17
+    '17': {
+        'label':'View Rental History of a Bike by ID',
+        'sql':  """SELECT R.* FROM Rentals R
+                    JOIN Rental_Bikes RB ON R.r_id = RB.rb_r_id
+                    WHERE RB.rb_b_id = ?""",
+        'params': ['Bike ID']
+    },
 }
